0913c12231bc2cb5c7d66443a4ab50eec1db9d04
Suso <susinodevelop@gmail.com>
2024-08-18 21:35:45 +0200

    Corrige color de fondo del status bar

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="light" />
+      <StatusBar style="light" backgroundColor="black"/>
       <CalculatorScreen />
     </SafeAreaView>
   );
